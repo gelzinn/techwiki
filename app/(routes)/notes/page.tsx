01ea32552c29684b4cbc26eab47ee021fd3489b2
gelzinn <mcnoobend@gmail.com>
2023-12-27 01:34:16 -0300

    fix(styles): refactor order of classNames in Notes page

--- app/(routes)/notes/page.tsx
+++ app/(routes)/notes/page.tsx
@@ -5,19 +5,19 @@ export default async function Notes() {
   const posts = await getNotes();
 
   return (
-    <main className="flex flex-col items-start justify-center w-full max-w-screen-full-hd mx-auto py-8 p-4">
-      <h1 className="flex items-center justify-start gap-4 w-full mb-8 font-medium pb-4 border-b border-zinc-200 dark:border-zinc-900">
+    <main className="mx-auto flex w-full max-w-screen-full-hd flex-col items-start justify-center p-4 py-8">
+      <h1 className="mb-8 flex w-full items-center justify-start gap-4 border-b border-zinc-200 pb-4 font-medium dark:border-zinc-900">
         <a
           href="/"
-          className="flex items-center justify-center gap-2 bg-zinc-100 border border-zinc-200 hover:bg-zinc-200 dark:bg-zinc-950 dark:border-zinc-900 dark:hover:bg-zinc-900 rounded-md p-2"
+          className="flex items-center justify-center gap-2 rounded-md border border-zinc-200 bg-zinc-100 p-2 hover:bg-zinc-200 dark:border-zinc-900 dark:bg-zinc-950 dark:hover:bg-zinc-900"
         >
           <span className="sr-only">Home</span>
-          <ChevronLeft className="w-4 h-4" />
+          <ChevronLeft className="h-4 w-4" />
         </a>
         <span className="text-3xl">All notes</span>
       </h1>
 
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-8 w-full">
+      <ul className="grid w-full grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {posts && posts.length > 0 ? (
           Array.from({
             length: 8,
@@ -38,20 +38,20 @@ export default async function Notes() {
               return (
                 <li
                   key={index}
-                  className="flex flex-col items-start justify-start w-full"
+                  className="flex w-full flex-col items-start justify-start"
                 >
-                  <picture className="w-full mb-4 overflow-hidden rounded-md aspect-video pointer-events-none select-none">
+                  <picture className="pointer-events-none mb-4 aspect-video w-full select-none overflow-hidden rounded-md">
                     <source srcSet={thumbnail} />
                     <source srcSet={thumbnail} />
                     <img
                       src={thumbnail}
                       alt={title}
-                      className="aspect-video pointer-events-none select-none"
+                      className="pointer-events-none aspect-video select-none"
                     />
                   </picture>
 
                   <section
-                    className="flex flex-wrap items-center justify-start w-full gap-2 mb-4"
+                    className="mb-4 flex w-full flex-wrap items-center justify-start gap-2"
                     aria-label="Categories"
                   >
                     {categories &&
@@ -59,7 +59,7 @@ export default async function Notes() {
                         return (
                           <span
                             key={index}
-                            className="inline-block px-2 py-1 text-xs font-medium tracking-tight uppercase bg-zinc-100 text-zinc-500 rounded-md dark:bg-zinc-900 dark:text-zinc-400"
+                            className="inline-block rounded-md bg-zinc-100 px-2 py-1 text-xs font-medium uppercase tracking-tight text-zinc-500 dark:bg-zinc-900 dark:text-zinc-400"
                           >
                             {category}
                           </span>
@@ -68,7 +68,7 @@ export default async function Notes() {
                   </section>
 
                   <section
-                    className="flex flex-col items-start justify-start w-full gap-1 mb-4"
+                    className="mb-4 flex w-full flex-col items-start justify-start gap-1"
                     aria-label="Title and description"
                   >
                     <strong className="w-full font-medium tracking-tight">
@@ -81,7 +81,7 @@ export default async function Notes() {
                   </section>
 
                   <section
-                    className="flex flex-wrap items-center justify-start w-full gap-2 mb-4"
+                    className="mb-4 flex w-full flex-wrap items-center justify-start gap-2"
                     aria-label="Authors"
                   >
                     {authors &&
@@ -103,7 +103,7 @@ export default async function Notes() {
                                   key={index}
                                   src={`https://github.com/${author}.png`}
                                   alt={author}
-                                  className="w-6 h-6 rounded-full pointer-events-none select-none"
+                                  className="pointer-events-none h-6 w-6 select-none rounded-full"
                                 />
 
                                 <span className="text-sm">{author}</span>
@@ -121,7 +121,7 @@ export default async function Notes() {
                                     key={index}
                                     src={`https://github.com/${author}.png`}
                                     alt={author}
-                                    className="w-6 h-6 rounded-full pointer-events-none select-none"
+                                    className="pointer-events-none h-6 w-6 select-none rounded-full"
                                   />
 
                                   <span className="text-sm">{author}</span>
@@ -145,7 +145,7 @@ export default async function Notes() {
                                     key={index}
                                     src={`https://github.com/${author}.png`}
                                     alt={author}
-                                    className="w-6 h-6 rounded-full pointer-events-none select-none"
+                                    className="pointer-events-none h-6 w-6 select-none rounded-full"
                                   />
 
                                   <span className="text-sm">{author}</span>
