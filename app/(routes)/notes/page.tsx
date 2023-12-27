7f42b602ea5d7ac4a9d6e351028f789aa1f660c1
gelzinn <mcnoobend@gmail.com>
2023-12-27 01:46:35 -0300

    fix: remove test posts array

--- app/(routes)/notes/page.tsx
+++ app/(routes)/notes/page.tsx
@@ -19,81 +19,87 @@ export default async function Notes() {
 
       <ul className="grid w-full grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {posts && posts.length > 0 ? (
-          Array.from({
-            length: 8,
-          })
-            .reverse()
-            .map((_, index) => {
-              const post: any = posts[0];
+          posts.map((post: any, index: number) => {
+            const { slug, title, description, thumbnail, categories, authors } =
+              post;
 
-              const {
-                slug,
-                title,
-                description,
-                thumbnail,
-                categories,
-                authors,
-              } = post;
+            return (
+              <li
+                key={index}
+                className="flex w-full flex-col items-start justify-start"
+              >
+                <picture className="pointer-events-none mb-4 aspect-video w-full select-none overflow-hidden rounded-md">
+                  <source srcSet={thumbnail} />
+                  <source srcSet={thumbnail} />
+                  <img
+                    src={thumbnail}
+                    alt={title}
+                    className="pointer-events-none aspect-video select-none"
+                  />
+                </picture>
 
-              return (
-                <li
-                  key={index}
-                  className="flex w-full flex-col items-start justify-start"
+                <section
+                  className="mb-4 flex w-full flex-wrap items-center justify-start gap-2"
+                  aria-label="Categories"
                 >
-                  <picture className="pointer-events-none mb-4 aspect-video w-full select-none overflow-hidden rounded-md">
-                    <source srcSet={thumbnail} />
-                    <source srcSet={thumbnail} />
-                    <img
-                      src={thumbnail}
-                      alt={title}
-                      className="pointer-events-none aspect-video select-none"
-                    />
-                  </picture>
+                  {categories &&
+                    categories.map((category: any, index: number) => {
+                      return (
+                        <span
+                          key={index}
+                          className="inline-block rounded-md bg-zinc-100 px-2 py-1 text-xs font-medium uppercase tracking-tight text-zinc-500 dark:bg-zinc-900 dark:text-zinc-400"
+                        >
+                          {category}
+                        </span>
+                      );
+                    })}
+                </section>
 
-                  <section
-                    className="mb-4 flex w-full flex-wrap items-center justify-start gap-2"
-                    aria-label="Categories"
-                  >
-                    {categories &&
-                      categories.map((category: any, index: number) => {
-                        return (
-                          <span
-                            key={index}
-                            className="inline-block rounded-md bg-zinc-100 px-2 py-1 text-xs font-medium uppercase tracking-tight text-zinc-500 dark:bg-zinc-900 dark:text-zinc-400"
-                          >
-                            {category}
-                          </span>
-                        );
-                      })}
-                  </section>
+                <section
+                  className="mb-4 flex w-full flex-col items-start justify-start gap-1"
+                  aria-label="Title and description"
+                >
+                  <strong className="w-full font-medium tracking-tight">
+                    {title}
+                  </strong>
 
-                  <section
-                    className="mb-4 flex w-full flex-col items-start justify-start gap-1"
-                    aria-label="Title and description"
-                  >
-                    <strong className="w-full font-medium tracking-tight">
-                      {title}
-                    </strong>
+                  <p className="w-full text-zinc-500 dark:text-zinc-400">
+                    {description}
+                  </p>
+                </section>
 
-                    <p className="w-full text-zinc-500 dark:text-zinc-400">
-                      {description}
-                    </p>
-                  </section>
+                <section
+                  className="mb-4 flex w-full flex-wrap items-center justify-start gap-2"
+                  aria-label="Authors"
+                >
+                  {authors &&
+                    authors.map((author: any, index: number) => {
+                      const quantity = authors.length;
 
-                  <section
-                    className="mb-4 flex w-full flex-wrap items-center justify-start gap-2"
-                    aria-label="Authors"
-                  >
-                    {authors &&
-                      authors.map((author: any, index: number) => {
-                        const quantity = authors.length;
+                      const isLast = index === quantity - 1;
+                      const isPenultimate = index === quantity - 2;
 
-                        const isLast = index === quantity - 1;
-                        const isPenultimate = index === quantity - 2;
+                      switch (quantity) {
+                        case 1:
+                          return (
+                            <a
+                              key={index}
+                              className="flex items-center justify-start gap-1"
+                              href={`notes?author=${author}`}
+                            >
+                              <img
+                                key={index}
+                                src={`https://github.com/${author}.png`}
+                                alt={author}
+                                className="pointer-events-none h-6 w-6 select-none rounded-full"
+                              />
 
-                        switch (quantity) {
-                          case 1:
-                            return (
+                              <span className="text-sm">{author}</span>
+                            </a>
+                          );
+                        case 2:
+                          return (
+                            <>
                               <a
                                 key={index}
                                 className="flex items-center justify-start gap-1"
@@ -108,75 +114,55 @@ export default async function Notes() {
 
                                 <span className="text-sm">{author}</span>
                               </a>
-                            );
-                          case 2:
-                            return (
-                              <>
-                                <a
+
+                              {!isLast && <span className="text-sm">and</span>}
+                            </>
+                          );
+                        default:
+                        case 3:
+                          return (
+                            <>
+                              <a
+                                key={index}
+                                className="flex items-center justify-start gap-1"
+                                href={`notes?author=${author}`}
+                              >
+                                <img
                                   key={index}
-                                  className="flex items-center justify-start gap-1"
-                                  href={`notes?author=${author}`}
-                                >
-                                  <img
-                                    key={index}
-                                    src={`https://github.com/${author}.png`}
-                                    alt={author}
-                                    className="pointer-events-none h-6 w-6 select-none rounded-full"
-                                  />
+                                  src={`https://github.com/${author}.png`}
+                                  alt={author}
+                                  className="pointer-events-none h-6 w-6 select-none rounded-full"
+                                />
 
-                                  <span className="text-sm">{author}</span>
-                                </a>
+                                <span className="text-sm">{author}</span>
+                              </a>
 
-                                {!isLast && (
-                                  <span className="text-sm">and</span>
-                                )}
-                              </>
-                            );
-                          default:
-                          case 3:
-                            return (
-                              <>
-                                <a
-                                  key={index}
-                                  className="flex items-center justify-start gap-1"
-                                  href={`notes?author=${author}`}
-                                >
-                                  <img
-                                    key={index}
-                                    src={`https://github.com/${author}.png`}
-                                    alt={author}
-                                    className="pointer-events-none h-6 w-6 select-none rounded-full"
-                                  />
+                              {!isLast && (
+                                <span className="-ml-2 text-sm">,</span>
+                              )}
 
-                                  <span className="text-sm">{author}</span>
-                                </a>
+                              {isPenultimate && (
+                                <span className="text-sm">and</span>
+                              )}
 
-                                {!isLast && (
-                                  <span className="-ml-2 text-sm">,</span>
-                                )}
+                              {isLast && (
+                                <span className="-ml-2 text-sm">.</span>
+                              )}
+                            </>
+                          );
+                      }
+                    })}
+                </section>
 
-                                {isPenultimate && (
-                                  <span className="text-sm">and</span>
-                                )}
-
-                                {isLast && (
-                                  <span className="-ml-2 text-sm">.</span>
-                                )}
-                              </>
-                            );
-                        }
-                      })}
-                  </section>
-
-                  <a
-                    href={`/notes/${slug}`}
-                    className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-300"
-                  >
-                    Read more →
-                  </a>
-                </li>
-              );
-            })
+                <a
+                  href={`/notes/${slug}`}
+                  className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-300"
+                >
+                  Read more →
+                </a>
+              </li>
+            );
+          })
         ) : (
           <li>No notes yet.</li>
         )}
