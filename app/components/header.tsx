ac910f23d1830b6c5b342437ba9806bb522f3085
gelzinn <mcnoobend@gmail.com>
2023-12-26 22:32:28 -0300

    fix: refactor header component to add TechWiki logo

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,19 +1,26 @@
 'use client';
 
+import { useState } from 'react';
 import { useTheme } from 'app/hooks/useTheme';
 
 import { Menu, Moon, PanelRightClose, Sun } from 'lucide-react';
+import { TechWikiLogo } from './logo';
 
 export const Header = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme, oppositeTheme, toggleTheme } = useTheme();
+
+  const handleToggleTheme = () => toggleTheme();
 
   return (
-    <header className="flex items-center justify-center bg-zinc-50 text-black dark:bg-black dark:text-zinc-50 border-b border-zinc-200 dark:border-zinc-900 mx-auto overflow-hidden">
-      <nav className="relative flex flex-row-reverse lg:flex-row items-center justify-between w-full max-w-screen-desktop h-16 px-8 py-4">
+    <header
+      className="flex items-center justify-center bg-zinc-50 text-black dark:bg-black dark:text-zinc-50 border-b border-zinc-200 dark:border-zinc-900 mx-auto overflow-hidden"
+      id="main-header"
+    >
+      <nav className="relative flex flex-row-reverse lg:flex-row items-center justify-between w-full max-w-screen-full-hd h-16 p-4">
         <button
           type="button"
           title="Toggle navigation menu"
-          className="p-2 rounded-md bg-zinc-200 dark:bg-zinc-950 border border-zinc-300 dark:border-zinc-900 hover:bg-zinc-300 dark:hover:bg-zinc-900"
+          className="flex items-center justify-center p-2 w-8 h-8 rounded-md bg-zinc-100 dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-900 hover:bg-zinc-200 dark:hover:bg-zinc-900"
         >
           <span className="sr-only">Toggle navigation menu</span>
 
@@ -25,18 +32,13 @@ export const Header = () => {
           <Menu className="block lg:hidden w-4 h-4" aria-hidden="true" />
         </button>
 
-        <h1 className="text-xl">
-          <a href="/">
-            <span className="font-medium">Tech</span>
-            <span className="font-light">Wiki</span>
-          </a>
-        </h1>
+        <TechWikiLogo className="text-xl" isLink />
 
         <button
           type="button"
-          title={`Toggle to ${theme === 'dark' ? 'light' : 'dark'} mode`}
-          className="block p-2 rounded-md bg-zinc-200 dark:bg-zinc-950 border border-zinc-300 dark:border-zinc-900 hover:bg-zinc-300 dark:hover:bg-zinc-900"
-          onClick={() => toggleTheme()}
+          title={`Toggle to ${oppositeTheme} mode`}
+          className="flex items-center justify-center p-2 w-8 h-8 rounded-md bg-zinc-100 dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-900 hover:bg-zinc-200 dark:hover:bg-zinc-900"
+          onClick={handleToggleTheme}
         >
           {theme === 'dark' ? (
             <Sun className="w-4 h-4" />
