c571edaeb61b387ad1b2b3c1fdf12befa49d167d
gelzinn <mcnoobend@gmail.com>
2023-12-28 22:54:31 -0300

    feat(styles): update notes layout

--- app/(routes)/notes/page.tsx
+++ app/(routes)/notes/page.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 import { Fragment } from 'react';
 
 import { getNotes } from 'app/lib/mdx';
-import { ChevronLeft } from 'lucide-react';
+import { ArrowRight, ChevronLeft } from 'lucide-react';
+import { IPost } from 'app/@types/post';
 
 export default async function Notes() {
   const posts = await getNotes();
@@ -22,16 +23,23 @@ export default async function Notes() {
         <span className="text-xl sm:text-3xl">All notes</span>
       </h1>
 
-      <ul className="grid w-full grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <ul className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {posts && posts.length > 0 ? (
-          posts.map((post: any, index: number) => {
-            const { slug, title, description, thumbnail, categories, authors } =
-              post;
+          posts.map((post: IPost, index: number) => {
+            const {
+              date,
+              slug,
+              title,
+              description,
+              thumbnail,
+              categories,
+              authors,
+            } = post;
 
             return (
               <li
                 key={index}
-                className="flex w-full flex-col items-start justify-start"
+                className="flex w-full flex-col items-start justify-start overflow-hidden"
               >
                 <Link
                   href={`/notes/${slug}`}
@@ -42,7 +50,7 @@ export default async function Notes() {
                       <Image
                         src={thumbnail}
                         alt={title}
-                        className="pointer-events-none aspect-video w-full select-none rounded-md bg-zinc-100 object-cover dark:bg-zinc-900"
+                        className="pointer-events-none aspect-video w-full select-none bg-zinc-100 object-cover dark:bg-zinc-900"
                         style={{ objectFit: 'cover' }}
                         width={640}
                         height={360}
@@ -76,73 +84,94 @@ export default async function Notes() {
                   )}
                 </section>
 
-                <section
-                  className="flex w-full flex-1 flex-col items-start justify-start gap-1"
-                  aria-label="Title and description"
+                <div
+                  className="flex w-full flex-col items-start justify-start"
+                  aria-label="Title, description and authors"
                 >
-                  <strong className="w-full font-medium tracking-tight">
-                    {title}
-                  </strong>
+                  <section
+                    className="flex w-full flex-col items-start justify-start gap-1"
+                    aria-label="Title and description"
+                  >
+                    <strong className="w-full font-medium tracking-tight">
+                      {title}
+                    </strong>
 
-                  <p className="h-full w-full text-sm text-zinc-500 dark:text-zinc-400">
-                    {description}
-                  </p>
-                </section>
+                    <p className="h-full w-full text-sm text-zinc-500 dark:text-zinc-400">
+                      {description}
+                    </p>
+                  </section>
 
-                <section
-                  className="my-4 flex w-full flex-wrap items-center justify-start gap-1.5"
-                  aria-label="Authors"
+                  <section
+                    className="my-4 flex w-full flex-wrap items-center justify-start gap-1.5"
+                    aria-label="Authors"
+                  >
+                    {authors && authors.length > 0 ? (
+                      authors.map((author: string, index: number) => (
+                        <Fragment key={index}>
+                          <Link
+                            className="flex items-center justify-start"
+                            href={`/notes?author=${author}`}
+                          >
+                            <Image
+                              src={`https://github.com/${author}.png`}
+                              alt={author}
+                              className="pointer-events-none mr-1 h-6 w-6 select-none rounded-full border border-zinc-200 bg-zinc-100 dark:border-zinc-900 dark:bg-zinc-900"
+                              width={24}
+                              height={24}
+                              loading="lazy"
+                              about={`Avatar of ${author}`}
+                            />
+
+                            <span className="text-sm">{author}</span>
+                          </Link>
+
+                          {index === authors.length - 2 && (
+                            <span className="pointer-events-none text-sm text-zinc-400">
+                              {' '}
+                              and
+                            </span>
+                          )}
+
+                          {index < authors.length - 2 && (
+                            <span className="pointer-events-none text-sm text-zinc-400">
+                              ,
+                            </span>
+                          )}
+                        </Fragment>
+                      ))
+                    ) : (
+                      <span
+                        className="text-sm text-zinc-500 dark:text-zinc-400"
+                        aria-label="Anonymous"
+                      >
+                        Anonymous author.
+                      </span>
+                    )}
+                  </section>
+                </div>
+
+                <footer
+                  className="flex w-full flex-col items-start justify-start gap-1.5"
+                  aria-label="Read more and published on"
                 >
-                  {authors && authors.length > 0 ? (
-                    authors.map((author: string, index: number) => (
-                      <Fragment key={index}>
-                        <Link
-                          className="flex items-center justify-start"
-                          href={`/notes?author=${author}`}
-                        >
-                          <Image
-                            src={`https://github.com/${author}.png`}
-                            alt={author}
-                            className="pointer-events-none mr-1 h-6 w-6 select-none rounded-full border border-zinc-200 bg-zinc-100 dark:border-zinc-900 dark:bg-zinc-900"
-                            width={24}
-                            height={24}
-                            loading="lazy"
-                            about={`Avatar of ${author}`}
-                          />
+                  <Link
+                    href={`/notes/${slug}`}
+                    className="group flex items-center justify-start gap-1.5 text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-300"
+                  >
+                    Read more <ArrowRight className="h-4 w-4" />
+                  </Link>
 
-                          <span className="text-sm">{author}</span>
-                        </Link>
-
-                        {index === authors.length - 2 && (
-                          <span className="pointer-events-none text-sm text-zinc-400">
-                            {' '}
-                            and
-                          </span>
-                        )}
-
-                        {index < authors.length - 2 && (
-                          <span className="pointer-events-none text-sm text-zinc-400">
-                            ,
-                          </span>
-                        )}
-                      </Fragment>
-                    ))
-                  ) : (
-                    <span
-                      className="text-sm text-zinc-500 dark:text-zinc-400"
-                      aria-label="Anonymous"
-                    >
-                      Anonymous author.
-                    </span>
-                  )}
-                </section>
-
-                <Link
-                  href={`/notes/${slug}`}
-                  className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-300"
-                >
-                  Read more →
-                </Link>
+                  <span
+                    className="text-sm text-zinc-500 dark:text-zinc-400"
+                    aria-label="Published on"
+                  >
+                    {new Date(date).toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })}
+                  </span>
+                </footer>
               </li>
             );
           })
