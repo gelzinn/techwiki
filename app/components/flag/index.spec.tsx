4c29f1dd535f8b7e7271479265f2426901d0e4c4
gelzinn <mcnoobend@gmail.com>
2023-12-29 03:07:19 -0300

    refactor(test): flag component tests and add mock data

--- app/components/flag/index.spec.tsx
+++ app/components/flag/index.spec.tsx
@@ -1,12 +1,43 @@
 import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { vi, afterEach, describe, expect, test, vitest } from 'vitest';
 
 import { Flag } from './index';
 
-const country_code = 'us';
+import { mock_data } from './mock-data';
+
+const countryCode = 'us';
+
+const mock_fetch = vitest.fn(async () => ({
+  json: () => Promise.resolve({}),
+}));
+
+const mockResponse = (status: number) => {
+  switch (status) {
+    case 200:
+      vi.spyOn(window, 'fetch').mockImplementationOnce(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(mock_data),
+        } as Response);
+      });
+      break;
+    case 404:
+      mock_fetch.mockResolvedValueOnce({
+        json: () => Promise.resolve({}),
+      });
+      break;
+    default:
+      mock_fetch.mockRejectedValueOnce({});
+  }
+};
 
 describe('Flag', () => {
+  afterEach(() => {
+    mock_fetch.mockClear();
+  });
+
   test('should render null if no country code is passed', () => {
+    mockResponse(404);
+
     const { container } = render(<Flag countryCode="" />);
     const flag = container.querySelector('i');
 
@@ -14,6 +45,8 @@ describe('Flag', () => {
   });
 
   test('should not render if no source is available', () => {
+    mockResponse(404);
+
     const { container } = render(<Flag countryCode="xx" />);
     const flag = container.querySelector('i');
 
@@ -21,16 +54,18 @@ describe('Flag', () => {
   });
 
   test('should render correctly', () => {
-    const { container } = render(<Flag countryCode={country_code} />);
+    mockResponse(200);
+
+    const { container } = render(<Flag countryCode={countryCode} />);
     const flag = container.querySelector('i');
 
     expect(flag).toBeDefined();
   });
 
   test('should render correctly with size', () => {
-    const { container } = render(
-      <Flag countryCode={country_code} size={100} />,
-    );
+    mockResponse(200);
+
+    const { container } = render(<Flag countryCode={countryCode} size={100} />);
     const flag = container.querySelector('i');
 
     if (!flag) return;
@@ -39,5 +74,16 @@ describe('Flag', () => {
 
     expect(flag).toBeDefined();
     expect(computed_style.width).toBe('100px');
+  });
+
+  test('should render correctly with id', () => {
+    mockResponse(200);
+
+    const { container } = render(<Flag countryCode={countryCode} />);
+    const flag = container.querySelector('i');
+
+    if (!flag) return;
+
+    expect(flag.id).toBe(`flag-${countryCode}`);
   });
 });
