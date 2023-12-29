96477be0770a5ad74c995899c2b58cd33f13da12
gelzinn <mcnoobend@gmail.com>
2023-12-29 02:12:08 -0300

    fix(styles): update footer text alignment

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer className="sticky top-0 z-50 mx-auto flex items-center justify-center overflow-hidden border-t border-zinc-200 bg-zinc-100 text-black dark:border-zinc-900 dark:bg-black dark:text-zinc-50">
       <div className="relative flex h-16 w-full max-w-screen-full-hd items-center justify-center p-4">
-        <span className="flex items-center justify-center text-sm text-zinc-500 dark:text-zinc-400 text-center">
+        <span className="flex items-center justify-center text-center text-sm text-zinc-500 dark:text-zinc-400">
           {currentYear} © TechWiki - All rights reserved.
         </span>
       </div>
