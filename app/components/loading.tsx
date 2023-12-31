23244a70641d571d3fd1c2d93d6b0a80a73e86f5
gelzinn <mcnoobend@gmail.com>
2023-12-31 04:18:16 -0300

    feat(components): add Loading component

---
+++ app/components/loading.tsx
@@ -0,0 +1,8 @@
+export const Loading = () => {
+  return (
+    <div
+      className="h-6 w-6 animate-spin rounded-full border-2 border-dashed border-gray-950 dark:border-gray-50"
+      role="status"
+    />
+  );
+};
