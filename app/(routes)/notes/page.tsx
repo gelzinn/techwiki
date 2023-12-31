c91f6cfbb15849db976179429da07dde68354b66
gelzinn <mcnoobend@gmail.com>
2023-12-31 18:19:30 -0300

    fix(styles): reorder CSS classes in note pages

--- app/(routes)/notes/page.tsx
+++ app/(routes)/notes/page.tsx
@@ -362,16 +362,16 @@ export default function Notes() {
       ) : (
         <>
           <div
-            className="mb-8 flex w-full h-16 items-center justify-start gap-4 border-b border-zinc-200 pb-4 font-medium dark:border-zinc-900 animate-pulse pointer-events-none select-none"
+            className="pointer-events-none mb-8 flex h-16 w-full animate-pulse select-none items-center justify-start gap-4 border-b border-zinc-200 pb-4 font-medium dark:border-zinc-900"
             aria-label="Loading notes"
           >
-            <div className="min-h-10 h-10 min-w-10 w-10 rounded-md bg-zinc-100 dark:bg-zinc-900" />
-            <div className="min-h-10 h-10 w-full max-w-md rounded-md bg-zinc-100 dark:bg-zinc-900" />
+            <div className="h-10 min-h-10 w-10 min-w-10 rounded-md bg-zinc-100 dark:bg-zinc-900" />
+            <div className="h-10 min-h-10 w-full max-w-md rounded-md bg-zinc-100 dark:bg-zinc-900" />
           </div>
 
           <div
             aria-label="Loading filters"
-            className="mb-8 flex w-full h-screen items-start justify-start gap-2 overflow-hidden animate-pulse pointer-events-none select-none"
+            className="pointer-events-none mb-8 flex h-screen w-full animate-pulse select-none items-start justify-start gap-2 overflow-hidden"
             style={{
               WebkitMaskImage: 'linear-gradient(to top, transparent, black)',
             }}
@@ -383,7 +383,7 @@ export default function Notes() {
               {Array.from({ length: 30 }).map((_, index: number) => (
                 <li
                   key={index}
-                  className="flex w-full min-h-48 h-fit aspect-video flex-col items-start justify-start overflow-hidden pb-4 pt-8 first-of-type:pt-0 sm:py-0 bg-zinc-100 dark:bg-zinc-900 rounded-md"
+                  className="flex aspect-video h-fit min-h-48 w-full flex-col items-start justify-start overflow-hidden rounded-md bg-zinc-100 pb-4 pt-8 first-of-type:pt-0 sm:py-0 dark:bg-zinc-900"
                 />
               ))}
             </ul>
