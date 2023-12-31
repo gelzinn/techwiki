e88fef6acfc01bbe02f7016baa99b902e5e7746a
gelzinn <mcnoobend@gmail.com>
2023-12-30 23:46:16 -0300

    feat: add preventComponents configuration to app config

---
+++ app/config/index.ts
@@ -0,0 +1,15 @@
+export const preventComponents = [
+  {
+    routes: [
+      '/auth',
+      '/404',
+      '/500',
+      '/maintenance',
+      '/coming-soon',
+      '/lock-screen',
+      '/forgot-password',
+      '/reset-password',
+    ],
+    components: ['Header', 'Footer'],
+  },
+];
