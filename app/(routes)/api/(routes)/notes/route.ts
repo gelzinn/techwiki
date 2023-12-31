1c41998c182673100f6423ab821f0b2edeab0249
gelzinn <mcnoobend@gmail.com>
2023-12-31 03:58:35 -0300

    feat(api): add note filtering by category

--- app/(routes)/api/(routes)/notes/route.ts
+++ app/(routes)/api/(routes)/notes/route.ts
@@ -5,10 +5,18 @@ import { repo, wikiPagesUrl as wikiUrl } from 'app/config/content';
 import { notes as notesEnv } from 'app/config/env';
 
 import { getNote } from '@/lib/notes';
+import { IPost } from '@/@types/post';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const author: string | null = searchParams.get('author' || '');
+
+  const params = {
+    author: searchParams.get('author') || null,
+    category: searchParams.get('category') || null,
+  };
+
+  const authors = params.author ? params.author.split(',') : null;
+  const categories = params.category ? params.category.split(',') : null;
 
   try {
     const res = await fetch(wikiUrl, {
@@ -43,20 +51,34 @@ export async function GET(request: Request) {
 
     let sortedNotes = [];
 
-    if (!author) {
+    if (!authors && !categories) {
       sortedNotes = notes.sort((a: any, b: any) => {
         const aDate = new Date(a.date);
         const bDate = new Date(b.date);
         return bDate.getTime() - aDate.getTime();
       });
     } else {
-      sortedNotes = notes
-        .filter((note: any) => note.authors.includes(author))
-        .sort((a: any, b: any) => {
-          const aDate = new Date(a.date);
-          const bDate = new Date(b.date);
-          return bDate.getTime() - aDate.getTime();
+      if (authors) {
+        authors.forEach((author) => {
+          const lowercaseAuthor = author.toLowerCase();
+          const authorNotes = notes.filter((note: IPost) =>
+            note.authors.map((a) => a.toLowerCase()).includes(lowercaseAuthor),
+          );
+          sortedNotes.push(...authorNotes);
         });
+      }
+
+      if (categories) {
+        categories.forEach((category) => {
+          const lowercaseCategory = category.toLowerCase();
+          const categoryNotes = notes.filter((note: IPost) =>
+            note.categories
+              .map((c) => c.toLowerCase())
+              .includes(lowercaseCategory),
+          );
+          sortedNotes.push(...categoryNotes);
+        });
+      }
     }
 
     return NextResponse.json({ data: sortedNotes }, { status: 200 });
