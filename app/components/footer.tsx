5e5eca0ccce66ccb07dcce5f93a383ac017ba487
gelzinn <mcnoobend@gmail.com>
2023-12-31 14:09:16 -0300

    feat: add GitHub Wiki link to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,7 +7,16 @@ export const Footer = () => {
     <footer className="sticky top-0 z-50 mx-auto flex items-center justify-center overflow-hidden border-t border-zinc-200 bg-zinc-100 text-black dark:border-zinc-900 dark:bg-black dark:text-zinc-50">
       <div className="relative flex h-16 w-full max-w-screen-full-hd items-center justify-center p-4">
         <span className="flex items-center justify-center text-center text-sm text-zinc-500 dark:text-zinc-400">
-          {currentYear} © TechWiki - All rights reserved.
+          {currentYear} © TechWiki - All rights reserved. Want to contribute?
+          Check out our
+          <a
+            target="_blank"
+            href="https://github.com/gelzinn/techwiki/wiki"
+            className="ml-1 text-zinc-600 dark:text-zinc-300 hover:underline"
+          >
+            GitHub Wiki
+          </a>
+          .
         </span>
       </div>
     </footer>
