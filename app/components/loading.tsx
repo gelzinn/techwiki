d4aa42fe834096dec5dd1a3c2384468606a1fdbe
gelzinn <mcnoobend@gmail.com>
2024-01-01 05:33:49 -0300

    fix: refactor Loading component to allow custom className

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -1,7 +1,12 @@
-export const Loading = () => {
+import { twMerge as cm } from 'tailwind-merge';
+
+export const Loading = ({ className }: { className?: string }) => {
   return (
     <div
-      className="h-6 w-6 animate-spin rounded-full border-2 border-dashed border-gray-950 dark:border-gray-50"
+      className={cm(
+        'h-6 w-6 animate-spin rounded-full border-2 border-dashed border-gray-950 dark:border-gray-50',
+        className,
+      )}
       role="status"
     />
   );
