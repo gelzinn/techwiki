8c56a43f77f569ce0ac89e6641158c8892189672
gelzinn <mcnoobend@gmail.com>
2024-01-02 12:26:20 -0300

    feat": add subnav configuration to notes page

--- app/(routes)/notes/page.tsx
+++ app/(routes)/notes/page.tsx
@@ -167,11 +167,11 @@ export default function Notes() {
   return (
     <main className="mx-auto flex h-auto min-h-screen w-full max-w-screen-full-hd flex-col items-start justify-start p-4">
       <section
-        className="flex items-center justify-between gap-2 w-full h-fit mb-4"
+        className="mb-4 flex h-fit w-full items-center justify-between gap-2"
         aria-label="Subnav and actions"
       >
         <div
-          className="flex items-center justify-start gap-4 w-fit h-fit"
+          className="flex h-fit w-fit items-center justify-start gap-4"
           aria-label="Subnav"
         >
           {subnav &&
@@ -183,12 +183,12 @@ export default function Notes() {
                 <Link
                   key={title}
                   href={href}
-                  className="relative flex items-center justify-center w-fit h-fit gap-2 rounded-md text-zinc-600 dark:text-zinc-400 px-1 py-2"
+                  className="relative flex h-fit w-fit items-center justify-center gap-2 rounded-md px-1 py-2 text-zinc-600 dark:text-zinc-400"
                 >
                   <span className="text-sm">{title}</span>
 
                   <div
-                    className="absolute opacity-0 data-[active=true]:opacity-100 bottom-0 left-0 right-0 h-px rounded-full bg-zinc-600 dark:bg-zinc-400 transition-all duration-300"
+                    className="absolute inset-x-0 bottom-0 h-px rounded-full bg-zinc-600 opacity-0 transition-all duration-300 data-[active=true]:opacity-100 dark:bg-zinc-400"
                     data-active={active}
                   />
                 </Link>
@@ -197,20 +197,20 @@ export default function Notes() {
         </div>
 
         <div
-          className="hidden sm:flex items-center justify-end gap-2 w-fit h-10"
+          className="hidden h-10 w-fit items-center justify-end gap-2 sm:flex"
           aria-label="Actions"
         >
-          <button className="flex items-center justify-center w-fit h-full gap-2 rounded-md border border-zinc-200 bg-zinc-100 p-2 hover:bg-zinc-200 dark:border-zinc-900 dark:bg-zinc-950 dark:hover:bg-zinc-900 text-zinc-600 dark:text-zinc-400">
+          <button className="flex h-full w-fit items-center justify-center gap-2 rounded-md border border-zinc-200 bg-zinc-100 p-2 text-zinc-600 hover:bg-zinc-200 dark:border-zinc-900 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-900">
             <Icon name="Filter" className="h-4 w-4" />
             <span className="text-sm">Filters</span>
           </button>
 
           <div
-            className="flex items-center justify-center w-fit h-full rounded-md border border-zinc-200 bg-zinc-100 dark:border-zinc-900 dark:bg-zinc-950 text-zinc-600 dark:text-zinc-400 overflow-hidden"
+            className="flex h-full w-fit items-center justify-center overflow-hidden rounded-md border border-zinc-200 bg-zinc-100 text-zinc-600 dark:border-zinc-900 dark:bg-zinc-950 dark:text-zinc-400"
             aria-label="Toggle view"
           >
             <button
-              className="flex items-center justify-center w-10 h-full gap-2 bg-zinc-100 p-2 hover:bg-zinc-200 dark:bg-zinc-950 dark:hover:bg-zinc-900 text-zinc-600 dark:text-zinc-400 data-[active=true]:bg-zinc-200 data-[active=true]:dark:bg-zinc-900"
+              className="flex h-full w-10 items-center justify-center gap-2 bg-zinc-100 p-2 text-zinc-600 hover:bg-zinc-200 data-[active=true]:bg-zinc-200 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-900 data-[active=true]:dark:bg-zinc-900"
               onClick={() => handleToggleView('grid')}
               data-active={selectedView === 'grid'}
             >
@@ -218,7 +218,7 @@ export default function Notes() {
             </button>
 
             <button
-              className="flex items-center justify-center w-10 h-full gap-2 bg-zinc-100 p-2 hover:bg-zinc-200 dark:bg-zinc-950 dark:hover:bg-zinc-900 text-zinc-600 dark:text-zinc-400 data-[active=true]:bg-zinc-200 data-[active=true]:dark:bg-zinc-900"
+              className="flex h-full w-10 items-center justify-center gap-2 bg-zinc-100 p-2 text-zinc-600 hover:bg-zinc-200 data-[active=true]:bg-zinc-200 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-900 data-[active=true]:dark:bg-zinc-900"
               onClick={() => handleToggleView('list')}
               data-active={selectedView === 'list'}
             >
@@ -227,7 +227,7 @@ export default function Notes() {
           </div>
 
           <button
-            className="flex items-center justify-center w-fit h-full gap-2 rounded-md border border-zinc-200 bg-zinc-100 p-2 hover:bg-zinc-200 dark:border-zinc-900 dark:bg-zinc-950 dark:hover:bg-zinc-900 text-zinc-600 dark:text-zinc-400 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-inherit"
+            className="flex h-full w-fit items-center justify-center gap-2 rounded-md border border-zinc-200 bg-zinc-100 p-2 text-zinc-600 hover:bg-zinc-200 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-inherit dark:border-zinc-900 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-900"
             disabled
           >
             <Icon name="Plus" className="h-4 w-4" />
@@ -302,7 +302,7 @@ export default function Notes() {
           <ul
             className={`${
               view === 'grid' ? 'grid gap-x-4 gap-y-8' : 'flex flex-col gap-0'
-            } w-full grid-cols-1 divide-y divide-zinc-200 sm:grid-cols-2 sm:divide-y-0 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 dark:divide-zinc-900 transition-all duration-500 ease-in-out`}
+            } w-full grid-cols-1 divide-y divide-zinc-200 transition-all duration-500 ease-in-out sm:grid-cols-2 sm:divide-y-0 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 dark:divide-zinc-900`}
           >
             {posts.length > 0 ? (
               posts
@@ -327,9 +327,9 @@ export default function Notes() {
                       key={index}
                       className={`flex w-full ${
                         view === 'grid'
-                          ? 'flex-col pb-0 sm:py-0 pt-8'
+                          ? 'flex-col pb-0 pt-8 sm:py-0'
                           : 'pb-12 pt-4'
-                      } items-start justify-start overflow-hidden first-of-type:pt-0 transition-all duration-500 ease-in-out`}
+                      } items-start justify-start overflow-hidden transition-all duration-500 ease-in-out first-of-type:pt-0`}
                     >
                       <div
                         className={`relative flex flex-col ${
@@ -339,14 +339,14 @@ export default function Notes() {
                         <Link
                           href={`/notes/${slug}`}
                           className={`flex ${
-                            view === 'grid' ? 'w-full mb-4' : 'w-fit'
+                            view === 'grid' ? 'mb-4 w-full' : 'w-fit'
                           } items-center justify-start gap-4 font-medium dark:border-zinc-900`}
                         >
                           <picture
                             className={`pointer-events-none aspect-video h-full w-full ${
                               view === 'grid'
                                 ? 'aspect-w-16 aspect-h-9'
-                                : 'min-w-96 max-w-96 aspect-w-16 aspect-h-9'
+                                : 'aspect-w-16 aspect-h-9 min-w-96 max-w-96'
                             } overflow-hidden rounded-md border border-zinc-200 bg-zinc-100 dark:border-zinc-900 dark:bg-zinc-900`}
                           >
                             {thumbnail ? (
@@ -378,8 +378,8 @@ export default function Notes() {
                         <section
                           className={`scrollbar-none flex items-center justify-start gap-1.5 ${
                             view === 'grid'
-                              ? 'w-full mb-4 overflow-x-auto'
-                              : 'absolute bottom-0 translate-y-full flex-wrap pt-4 mt-4'
+                              ? 'mb-4 w-full overflow-x-auto'
+                              : 'absolute bottom-0 mt-4 translate-y-full flex-wrap pt-4'
                           }`}
                           aria-label="Categories"
                         >
@@ -402,7 +402,7 @@ export default function Notes() {
                       </div>
 
                       <div
-                        className={`flex flex-1 w-full flex-col items-start justify-start ${
+                        className={`flex w-full flex-1 flex-col items-start justify-start ${
                           view === 'grid' ? '' : 'ml-4'
                         }`}
                         aria-label="Title, description and authors"
@@ -471,7 +471,7 @@ export default function Notes() {
 
                           {view === 'list' && (
                             <footer
-                              className={`flex w-full h-fit flex-col items-start justify-start gap-1.5`}
+                              className={`flex h-fit w-full flex-col items-start justify-start gap-1.5`}
                               aria-label="Read more and published on"
                             >
                               <Link
@@ -503,7 +503,7 @@ export default function Notes() {
 
                       {view === 'grid' && (
                         <footer
-                          className={`flex w-full h-fit flex-col items-start justify-start gap-1.5`}
+                          className={`flex h-fit w-full flex-col items-start justify-start gap-1.5`}
                           aria-label="Read more and published on"
                         >
                           <Link
---
+++ app/config/header/subnav.ts
@@ -0,0 +1,14 @@
+export const subnav = [
+  {
+    title: 'Recents',
+    href: '/notes',
+  },
+  {
+    title: 'Following',
+    href: '/notes',
+  },
+  {
+    title: 'Popular',
+    href: '/notes',
+  },
+];
