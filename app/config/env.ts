81254772400fe1d6f03014805001505eb1d94756
gelzinn <mcnoobend@gmail.com>
2024-01-02 23:02:55 -0300

    feat: add environment configuration and redirects

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,7 +1,18 @@
+type TEnv = 'dev' | 'prod';
+
+const environment: TEnv = (process.env.ENVIRONMENT as TEnv) || 'dev';
+
+const url = {
+  dev: 'http://localhost:3000',
+  prod: 'https://wiki.gelzin.com',
+};
+
+const baseUrl = url[environment];
+
 const notes = {
   revalidate: process.env.REVALIDATE_NOTES
     ? parseInt(process.env.REVALIDATE_NOTES)
     : 60 * 60, // in seconds
 };
 
-export { notes };
+export { environment, notes, url, baseUrl };
---
+++ app/config/redirects.ts
@@ -0,0 +1,6 @@
+export const redirects = {
+  '/about': 'https://github.com/gelzinn/techwiki/wiki',
+  '/github': 'https://github.com/gelzinn/techwiki',
+  '/contribute':
+    'https://github.com/gelzinn/techwiki?tab=readme-ov-file#readme',
+};
