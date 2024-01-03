6d9001c11ac2f211b66edf954f749078461fb888
gelzinn <mcnoobend@gmail.com>
2024-01-03 02:46:22 -0300

    fix: disable login and signup buttons temporarily

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -116,14 +116,16 @@ export const Header = () => {
 
           <button
             type="button"
-            className="flex h-10 w-auto items-center justify-center rounded-md border border-zinc-200 bg-zinc-100 px-4 py-2 text-sm text-zinc-600 hover:bg-zinc-200 dark:border-zinc-900 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-900"
+            className="flex h-10 w-auto items-center justify-center rounded-md border border-zinc-200 bg-zinc-100 px-4 py-2 text-sm text-zinc-600 hover:bg-zinc-200 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-900 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-900"
+            disabled
           >
             Login
           </button>
 
           <button
             type="button"
-            className="hidden h-10 w-auto items-center justify-center text-nowrap rounded-md border border-rose-400 bg-rose-500 px-6 py-2 text-sm text-zinc-50 sm:flex"
+            className="hidden h-10 w-auto items-center justify-center text-nowrap rounded-md border border-rose-400 bg-rose-500 px-6 py-2 text-sm text-zinc-50 hover:bg-rose-400 disabled:cursor-not-allowed disabled:opacity-50 sm:flex dark:border-rose-500 dark:bg-rose-600 dark:hover:bg-rose-500"
+            disabled
           >
             Sign up
           </button>
