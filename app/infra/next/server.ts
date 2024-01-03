86c35834662cc6ae3727bcfaa51913659a76ef51
gelzinn <mcnoobend@gmail.com>
2024-01-02 21:30:01 -0300

    feat(infra): remove NextRequest import to import your own type

--- app/infra/next/server.ts
+++ app/infra/next/server.ts
@@ -1,3 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export { NextResponse, NextRequest };
